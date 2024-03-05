3ba75de450fed05ab608e294ac9845bcd1764a7e
Patricia Medina anglada <pmedina-@c3r13s5.42madrid.com>
2024-03-05 21:39:10 +0100

    ex04 fixed

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -3,11 +3,10 @@ import sys
 try:
     assert not len(sys.argv) > 2, "more than one argument is provided"
     assert not len(sys.argv) < 2, "one argument needed"
-    try:
-        num = int(sys.argv[1])
-    except:
-        raise AssertionError("argument is not an integer")
+    num = int(sys.argv[1])
     whatis = "even" if num % 2 == 0 else "odd"
     print(f"I'm {whatis}")
 except AssertionError as ae:
     print(f"AssertionError: {ae}")
+except ValueError as ve:
+    print(f"AssertionError: argument is not an integer")
